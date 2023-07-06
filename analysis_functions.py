32e0ba2f4349e6cad56240983f3c2de5949fc388
Jared Sofair <sofairj@lafayette.edu>
2023-07-06 17:04:37 -0600

    Can now flag infinite kurtosis in the time-averaged power spectra.

--- analysis_functions.py
+++ analysis_functions.py
@@ -15,7 +15,7 @@ import seaborn as sns
 
 def plot_tavg_power(wf_in,
                     f_start=0, f_stop=6000,
-                    p_start=0, p_stop=5*10**10):
+                    p_start=0, p_stop=5*10**10, find_inf=False, n_divs=0):
 
 # def plot_tavg_power(wf_in, t=0,
 #                     f_start=np.amin(wf_in.get_freqs()), f_stop=np.amax(wf_in.get_freqs()),
@@ -31,16 +31,60 @@ def plot_tavg_power(wf_in,
     wf_pwr_mean = np.mean(wf_in.data, axis=0)
     wf_pwr_mean_integration = wf_pwr_mean[0]
     
-    fig, ax = plt.subplots()
-    ax.set_xlim(f_start, f_stop)
-    ax.set_ylim(p_start, p_stop)
-    ax.set_xlabel('Frequency (MHz)')
-    ax.set_ylabel('Time-Averaged Power (???)')
+    if find_inf == True:
+        # Get power and frequency in increasing order
+        if wf_in.header['foff'] < 0:
+            pows_flipped = np.flip(wf_in.data)
+            freqs_flipped = wf_in.get_freqs()[::-1]
 
-    ax.plot(wf_in.get_freqs(), wf_pwr_mean_integration)
-    plt.show()
-    plt.close()
+        # Time-average the power
+        pows_mean_flipped = np.mean(pows_flipped, axis=0)[0]    
 
+        # Split frequency and time-averaged power into n_divs channels
+        freqs = np.array_split(freqs_flipped, n_divs)
+        pows_mean = np.array_split(pows_mean_flipped, n_divs)
+
+        # Find kurtosis of each channel
+        kurts_list = []
+        for i, division in enumerate(pows_mean):
+            kurts_list.append(kurtosis(division))
+
+        kurts = np.array(kurts_list, dtype=np.float64)
+
+        # Bin frequencies
+        bins = []
+        for chnl in freqs:
+            bins.append(np.floor(chnl[0]))
+        
+
+        # Plot time-averaged power with frequencies of infinite kurtosis flagged
+        fig, ax = plt.subplots()
+        ax.set_xlim(f_start, f_stop)
+        ax.set_ylim(p_start, p_stop)
+        ax.set_xlabel('Frequency (MHz)')
+        ax.set_ylabel('Time-Averaged Power (Counts)')
+
+        ax.plot(wf_in.get_freqs(), wf_pwr_mean_integration)
+        
+        # Find where kurtosis is inf and plot these channels
+        inf_kurt = np.where(np.isfinite(kurts) == False)
+
+        inf_kurts = []
+        for i in inf_kurt[0]:
+            inf_kurts.append(bins[i])
+            plt.axvline(x=bins[i], ymin=0, ymax=10, c='r', ls='--')
+
+        plt.show()
+    else:
+        # Plot time-averaged power only
+        fig, ax = plt.subplots()
+        ax.set_xlim(f_start, f_stop)
+        ax.set_ylim(p_start, p_stop)
+        ax.set_xlabel('Frequency (MHz)')
+        ax.set_ylabel('Time-Averaged Power (Counts)')
+
+        ax.plot(wf_in.get_freqs(), wf_pwr_mean_integration)
+        plt.show()
 
     
 
